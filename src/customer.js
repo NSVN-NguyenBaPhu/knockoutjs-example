23471d30156ade734d225944e6b806e07e307a1c
Nguyen Ba Phu <phunb@nsvc.com>
2020-03-03 17:25:02 +0700

    add func add+remove customer

--- src/customer.js
+++ src/customer.js
@@ -7,16 +7,37 @@ function Customer(id, name, address) {
 }
 // ViewModel
 var CustomerVM = function () {
-    this.customers = ko.observableArray([
+    var vm = this;
+    vm.customer = new Customer(null, "", "");
+    vm.maxId = 3;
+    vm.customers = ko.observableArray([
         new Customer(1, "Phú NB", "HT1"),
         new Customer(2, "Sirius", "HN2")
     ]);
-
-    this.addCus = function () {
-        let curId = this.customers().length + 1;
-        this.customers.push(new Customer(curId, "New Customer" + curId, "HT" + curId));
+    vm.refreshData = function () {
+        vm.customer.id(null);
+        vm.customer.name("");
+        vm.customer.address("");
     }
-    
+    vm.refreshData();
+    vm.addCus = function () {
+        let curId = vm.maxId++;
+        // let curId = vm.getIdForAddCus() + 1;
+        let cusAdd = ko.toJS(vm.customer);
+        cusAdd.id = curId;
+        vm.customers.push(cusAdd);
+        vm.refreshData();
+    }
+    vm.removeCus = function (id) {
+        id = 2;
+        var index = vm.customers().map(c => c.id).indexOf(id);
+        var index2 = vm.customers().map(c => c.name).indexOf("Sirius");
+        vm.customers().splice(index, 1);
+    }
+    vm.getIdForAddCus = function () {
+        return Math.max.apply(Math, vm.customers().map(function (c) { return c.id; }))
+    }
+
 }
 
 ko.applyBindings(new CustomerVM());
